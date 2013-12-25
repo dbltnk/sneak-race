ba8f8efec6c83caa99882752ec06456839c04a8c
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-12-25 21:26:49 +0100

    working chars

---
+++ Assets/CharacterManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterManager : MonoBehaviour {
+
+	public bool hasCommand = false;
+	public bool isWalking = false;
+
+	// Use this for initialization
+	void Start () {
+		float r = Random.Range(0f,1f);
+		float g = Random.Range(0f,1f);
+		float b = Random.Range(0f,1f);
+		renderer.material.color = new Vector4(r,g,b,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isWalking) {
+			transform.Translate(Vector3.right * Time.deltaTime * GameManager.walkSpeed);
+		}
+
+		if (this.hasCommand == false) {
+			StartCoroutine("pickCommand"); 
+		}
+	}
+
+	IEnumerator pickCommand() {
+		this.hasCommand = true;
+		float time = Random.Range (GameManager.minWalkDuration, GameManager.maxWalkDuration);
+		float randomNumber = Random.Range(-1f,1f);
+		if (randomNumber <=0) {
+			this.isWalking = true;
+		}
+		else {
+			this.isWalking = false;
+		}
+		yield return new WaitForSeconds(time);
+		this.hasCommand = false;
+		this.isWalking = false;
+//		}
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject character;
+	public int numberOfChars;
+	public static float walkSpeed = 0.5f;
+	public static float runSpeed = 1f;
+	public static float minWalkDuration = 0.05f;
+	public static float maxWalkDuration = 1f; 
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < numberOfChars; i++) {
+			Instantiate(character, new Vector3(-5, -4.5f + i, 0), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
