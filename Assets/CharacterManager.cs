64c945c56c03a1370d50bd61d0a78e3896acd385
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-12-27 22:51:24 +0100

    refactored character

--- Assets/CharacterManager.cs
+++ Assets/CharacterManager.cs
@@ -24,7 +24,7 @@ public class CharacterManager : MonoBehaviour {
 			}
 		}
 		else {
-			playerInput();
+			playerInput(this.tag);
 		}
 
 		if (this.isWalking) {
@@ -54,62 +54,18 @@ public class CharacterManager : MonoBehaviour {
 //		}
 	}
 
-	void playerInput() {
-		if (this.tag == "1") {
-			if (Input.GetButton("A_1") || Input.GetButton("B_1")) {
-				this.isWalking = true;
-				this.isRunning = false;
-			}
-			else if (Input.GetButton("X_1") || Input.GetButton("Y_1")) {
-				this.isWalking = false;
-				this.isRunning = true;
-			}
-			else {
-				this.isWalking = false;
-				this.isRunning = false;
-			}
+	void playerInput(string tag) {
+		if (Input.GetButton("A_" + tag) || Input.GetButton("B_" + tag)) {
+			this.isWalking = true;
+			this.isRunning = false;
 		}
-		else if (this.tag == "2") {
-			if (Input.GetButton("A_2") || Input.GetButton("B_2")) {
-				this.isWalking = true;
-				this.isRunning = false;
-			}
-			else if (Input.GetButton("X_2") || Input.GetButton("Y_2")) {
-				this.isWalking = false;
-				this.isRunning = true;
-			}
-			else {
-				this.isWalking = false;
-				this.isRunning = false;
-			}
+		else if (Input.GetButton("X_" + tag) || Input.GetButton("Y_" + tag)) {
+			this.isWalking = false;
+			this.isRunning = true;
 		}
-		else if (this.tag == "3") {
-			if (Input.GetButton("A_3") || Input.GetButton("B_3")) {
-				this.isWalking = true;
-				this.isRunning = false;
-			}
-			else if (Input.GetButton("X_3") || Input.GetButton("Y_3")) {
-				this.isWalking = false;
-				this.isRunning = true;
-			}
-			else {
-				this.isWalking = false;
-				this.isRunning = false;
-			}
-		}
-		else if (this.tag == "4") {
-			if (Input.GetButton("A_4") || Input.GetButton("B_4")) {
-				this.isWalking = true;
-				this.isRunning = false;
-			}
-			else if (Input.GetButton("X_4") || Input.GetButton("Y_4")) {
-				this.isWalking = false;
-				this.isRunning = true;
-			}
-			else {
-				this.isWalking = false;
-				this.isRunning = false;
-			}
+		else {
+			this.isWalking = false;
+			this.isRunning = false;
 		}
 	}
 }
